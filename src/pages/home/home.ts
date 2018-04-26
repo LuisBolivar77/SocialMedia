5619bcc57902bbdc6d44117648fcc27c4332c291
LuchoBolivar <tennluis77@gmail.com>
2018-04-26 12:15:32 -0500

    primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Alert, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
+  }
+
+  like(){
+    let alert = this.alertCtrl.create({
+      title: 'Has dado un like',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  comentario(){
+    let alert = this.alertCtrl.create({
+      title: 'Has dado agragado un comentario',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  compartir(){
+    let alert = this.alertCtrl.create({
+      title: 'Has compartido esta publicacion',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
